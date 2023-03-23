b97bfb0b005a5a6470304e69598b79b12b030689
wzsamuels <wzsamuels@gmail.com>
2023-03-23 19:35:57 -0400

    Adjust layout for banner image on About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,12 +3,13 @@ import exterior_image from '../../public/exterior.png'
 
 const About = () => {
   return (
-    <div className={'mt-6 mb-6 p-4 w-full flex flex-col justify-center items-center'}>
-      <h1 className={'text-3xl md:text-4xl text-center font-serif'}>About Us</h1>
-      <h2 className={'my-6 text-xl md:text-2xl text-center font-serif'}>Proudly Serving Raleigh&apos;s Medical Needs</h2>
-
+    <div className={'mt-6 mb-6 w-full flex flex-col justify-center items-center'}>
+      <div className={'p-4'}>
+        <h1 className={'text-3xl md:text-4xl text-center font-serif'}>About Us</h1>
+        <h2 className={'my-6 text-xl md:text-2xl text-center font-serif'}>Proudly Serving Raleigh&apos;s Medical Needs</h2>
+      </div>
       <div className={'relative w-full max-w-5xl min-h-[400px]'}>
-        <Image fill className={'p-4 w-full object-cover'} src={exterior_image} alt={'Best American Medical Supplies exterior'}/>
+        <Image fill className={'w-full object-cover'} src={exterior_image} alt={'Best American Medical Supplies exterior'}/>
       </div>
     </div>
   )
