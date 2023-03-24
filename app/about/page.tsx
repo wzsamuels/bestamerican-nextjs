29b0511f436b14ffdda3e21c1022198ed156b4b6
wzsamuels <wzsamuels@gmail.com>
2023-03-23 20:06:42 -0400

    Add additional text to About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,8 @@ const About = () => {
     <div className={'mt-6 mb-6 w-full flex flex-col justify-center items-center'}>
       <div className={'p-4'}>
         <h1 className={'text-3xl md:text-4xl text-center font-serif'}>About Us</h1>
-        <h2 className={'my-6 text-xl md:text-2xl text-center font-serif'}>Proudly Serving Raleigh&apos;s Medical Needs</h2>
+        <h2 className={'my-6 text-xl md:text-2xl text-center font-serif'}>Best American is Proudly Serving Raleigh&apos;s Medical Supply Needs</h2>
+        <p>We have something for customers of all ages and medical needs. All our products are of the highest quality, fairly priced, and guaranteed to meet your expectations.</p>
       </div>
       <div className={'relative w-full max-w-5xl min-h-[400px]'}>
         <Image fill className={'w-full object-cover'} src={exterior_image} alt={'Best American Medical Supplies exterior'}/>
