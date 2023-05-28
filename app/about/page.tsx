4c39352f008656fe719604c931fafde1f48ad261
wzsamuels <wzsamuels@gmail.com>
2023-05-28 12:41:09 -0400

    Add new text to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ const About = () => {
       <div className={'p-4'}>
         <h1 className={'text-3xl md:text-4xl text-center font-serif'}>About Us</h1>
         <h2 className={'my-6 text-xl md:text-2xl text-center font-serif'}>Best American is Proudly Serving Raleigh&apos;s Medical Supply Needs</h2>
-        <p>We have something for customers of all ages and medical needs. All our products are of the highest quality, fairly priced, and guaranteed to meet your expectations.</p>
+        <p>Established in 2020, Best American Medical Supplies offers home respiratory and durable medical equipment supplies and services. We are accredited by the Accreditation Commission for Health Care (ACHC) and Medicaid and Medicare contracted Durable Medical Equipment (DME) partners. We strive to be our customer’s, and patient’s best option for durable home medical equipment and supplies; we value every order, and endeavor to provide excellent services all the time.</p>
       </div>
       <div className={'relative w-full max-w-5xl min-h-[400px]'}>
         <Image fill className={'w-full object-cover'} src={exterior_image} alt={'Best American Medical Supplies exterior'}/>
